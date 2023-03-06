addd8f5fa763ee7ec3f185f78ddbd3a1ecf8c7a2
leno-tabJ0728 <116152759+leno-tabJ0728@users.noreply.github.com>
2023-03-06 16:24:11 +0500

    INFRA : Upload Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("\nIsmingizni kiriting : ");
+string ism =Console.ReadLine();
+Console.WriteLine($"\nAssalomu aleykum!\n\nXush kelibsiz {ism}\n");
+Console.Write($"{ism} tug'ulgan yilingizni kiriting : ");
+int tyil=Convert.ToInt32(Console.ReadLine());
+int yosh = 2023 - tyil;
+Console.WriteLine($"\nHurmatli {ism} yoshingiz {yosh} da!");
+string info=$"\n{ism} siz Kescha nomli qush borligini bilasizmi!\n";
+Console.WriteLine($"{info}\nKescha va sizning yoshingiz farqini natijada ko'ring!\n");
+int k_age = 3;
+int Age=yosh-k_age;
+Console.WriteLine($"{ism} ning yoshi Kescha yoshidan {Age} yoshga katta!\n");
